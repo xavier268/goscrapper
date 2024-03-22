60f8bec4f50b5b76fd37ac0a2a48f8dcf8b91204
xavier@DellXPSA <xavier@DellXPSA>
2024-03-22 20:23:03 +0100

    input-outpu arguments

--- parser/parserSupport.go
+++ parser/parserSupport.go
@@ -5,13 +5,17 @@ import (
 	"strings"
 )
 
+// ===========================================================================================
 // When generating code, global state is kept within myLexer.
 // It can be reached from code in the grammar file using lex.(*myLexer).doSomething() ...
+// ===========================================================================================
 
+// add lines to the code to be generated.
 func (m *myLexer) addLines(lines ...string) {
 	m.lines = append(m.lines, lines...)
 }
 
+// finalize code generation, and write it to the output file.
 func (m *myLexer) finalize() {
 
 	// imports
@@ -24,20 +28,50 @@ func (m *myLexer) finalize() {
 	}
 	fmt.Fprintln(m.w, ")")
 	fmt.Fprintln(m.w)
+
+	// write type of inputParams
 	fmt.Fprintln(m.w)
+	fmt.Fprintf(m.w, "type %s struct {\n", "Input_"+m.name)
+	for _, l := range m.inparams {
+		fmt.Fprintf(m.w, "\t%s\n", l)
+	}
+	fmt.Fprintln(m.w, "}")
+	fmt.Fprintln(m.w)
+
+	// wrtite type of output parameters
+	fmt.Fprintln(m.w)
+	fmt.Fprintf(m.w, "type %s struct {\n", "Output_"+m.name)
+	for _, l := range m.outparams {
+		fmt.Fprintf(m.w, "\t%s\n", l)
+	}
+	fmt.Fprintln(m.w, "}")
 	fmt.Fprintln(m.w)
 
 	// write function header, with parameters
-	fmt.Fprintf(m.w, "\nfunc Do%s(%s) (%s) {\n", m.name, strings.Join(m.inparams, ","), strings.Join(m.outparams, ", "))
+	fmt.Fprintf(m.w, "\nfunc Do_%s(_in Input_%s) (%s, _err error) {\n", m.name, m.name, "_out []Output_"+m.name)
 
 	// write function body
 	for _, l := range m.lines {
 		fmt.Fprintf(m.w, "%s\n", l)
 	}
-	fmt.Fprintln(m.w, "}")
-	// possibly, save the source function at the bottom of the file
+
+	fmt.Fprintln(m.w, "return _out, _err\n}")
+
+	// save the source function at the bottom of the file
+	m.printCommentedSource()
 }
 
+// define an input parameter.
 func (m *myLexer) setParam(name string, typ string) {
 	m.inparams = append(m.inparams, name+" "+typ)
+}
+
+// print the data source used to create function, as a comment.
+func (m *myLexer) printCommentedSource() {
+	fmt.Fprintln(m.w)
+	dd := strings.Split(string(m.data), "\n")
+	for _, d := range dd {
+		fmt.Fprintf(m.w, "// %s\n", d)
+	}
+	fmt.Fprintln(m.w)
 }

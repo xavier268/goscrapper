cc852fc77fe7d65b2fda04714071bc0430c1fbac
xavier@Envy17 <xavier@Envy17>
2024-04-05 12:35:39 +0200

    renamed and complete cli

--- gsc/main.go
+++ gsc/main.go
@@ -16,21 +16,23 @@ var (
 	flagVersion = flag.Bool("version", false, "print version and exit")
 	flagInfo    = flag.Bool("info", false, "print info and exit")
 	flagHelp    = flag.Bool("help", false, "print help and exit")
-	outDir      string
+	flagAsync   = flag.Bool("async", false, "generate async mode api")
+	flagOutdir  = flag.String("outdir", "autoscrapper", "output directory")
 )
 
 func main() {
 
 	// define shorter flags
-	flag.BoolVar(flagVersion, "v", false, "print version and exit")
-	flag.BoolVar(flagInfo, "i", false, "print info and exit")
-	flag.BoolVar(flagHelp, "h", false, "print help and exit")
-	flag.StringVar(&outDir, "out", "autoscrapper", "output directory")
-	flag.StringVar(&outDir, "o", "autoscrapper", "output directory")
+	flag.BoolVar(flagVersion, "v", false, "")
+	flag.BoolVar(flagInfo, "i", false, "")
+	flag.BoolVar(flagHelp, "h", false, "")
+	flag.StringVar(flagOutdir, "o", "autoscrapper", "")
+	flag.BoolVar(flagAsync, "a", false, "")
 
 	// redefine Usage function to print default values.
 	flag.Usage = func() {
 		fmt.Printf("Usage of %s\n%s [ option flags ... ] inputFile1 inputFile2 inputFile3 ...\n", os.Args[0], filepath.Base(os.Args[0]))
+		fmt.Println("NB : file names can be glob patterns.")
 		flag.PrintDefaults()
 	}
 
@@ -58,11 +60,28 @@ func main() {
 		return
 
 	default:
-
-		err := parser.ParseFiles(outDir, flag.Args()...)
-		if err != nil {
-			panic(err)
+		if flagOutdir == nil || *flagOutdir == "" {
+			return
+		}
+		outDir := *flagOutdir
+		if len(flag.Args()) == 0 {
+			flag.Usage()
+			return
 		}
 
+		for _, fn := range flag.Args() {
+
+			if *flagAsync {
+				err := parser.ParseGlobAsync(outDir, fn)
+				if err != nil {
+					panic(err)
+				}
+			} else {
+				err := parser.ParseGlob(outDir, fn)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
 	}
 }
--- version.go
+++ version.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	VERSION   = "0.3.4"
+	VERSION   = "0.3.5"
 	COPYRIGHT = "(c) Xavier Gandillot 2024"
 )
 
